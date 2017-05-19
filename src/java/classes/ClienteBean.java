88f4fbb6a3c66ef24a6a05099be7d73052380b19
0571156 <0571156@200.132.53.57>
2017-05-19 11:17:55 -0300

    Commit SessionScoped

--- src/java/classes/ClienteBean.java
+++ src/java/classes/ClienteBean.java
@@ -6,17 +6,15 @@
 package classes;
 
 import java.util.List;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author marcos
  */
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class ClienteBean {
 
     private Cliente client = new Cliente();
